7d2998ee092febda5d62b90e326507ca9bf77146
Faiz Ahmed <faiz227@proton.me>
2024-09-19 22:18:11 +0530

    Functions 3 args

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,8 +2,13 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5);
+    print_labeled_measurement(60, 'm');
 }
 
 fn another_function(x: i32) {
     println!("Value of x = {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
